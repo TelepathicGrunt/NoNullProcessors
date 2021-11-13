beb6840d541fdefa7d6f736a04215643fa8213cc
TelepathicGrunt <alexsarnese@gmail.com>
2021-11-12 23:04:48 -0500

    added code from friend who helped find the fix

---
+++ src/main/java/com/telepathicgrunt/no_null_processors/mixin/RegistryReadOpsMixin.java
@@ -0,0 +1,28 @@
+package com.telepathicgrunt.no_null_processors.mixin;
+
+import com.mojang.serialization.DynamicOps;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.resources.RegistryReadOps;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+@Mixin(RegistryReadOps.class)
+public class RegistryReadOpsMixin {
+    @Unique
+    private static final Map<ResourceManager, RegistryReadOps<?>> cachedOps = new WeakHashMap<>();
+
+    @Inject(method = "create(Lcom/mojang/serialization/DynamicOps;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/core/RegistryAccess;)Lnet/minecraft/resources/RegistryReadOps;", at = @At("HEAD"), cancellable = true)
+    private static <T> void cacheOpsForPoolElementStructurePiece(DynamicOps<T> ops, ResourceManager manager, RegistryAccess registryAccess, CallbackInfoReturnable<RegistryReadOps<T>> cir) {
+        if (ops == NbtOps.INSTANCE) {
+            cir.setReturnValue((RegistryReadOps<T>) cachedOps.computeIfAbsent(manager, key -> RegistryReadOps.create(ops, manager, registryAccess)));
+        }
+    }
+}

423336a6c4c5a1a29145f9d5d6f35580119ba15b
TelepathicGrunt <alexsarnese@gmail.com>
2021-12-04 15:38:52 -0500

    ported to 1.18 forge

--- src/main/java/com/telepathicgrunt/no_null_processors/NoNullProcessors.java
+++ src/main/java/com/telepathicgrunt/no_null_processors/NoNullProcessors.java
@@ -1,15 +1,14 @@
 package com.telepathicgrunt.no_null_processors;
 
-import net.fabricmc.api.ModInitializer;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class NoNullProcessors implements ModInitializer {
+@Mod(NoNullProcessors.MODID)
+public class NoNullProcessors {
 
     public static final String MODID = "no_null_processors";
     public static final Logger LOGGER = LogManager.getLogger(MODID);
-    @Override
-    public void onInitialize() {}
+
+    public NoNullProcessors() {}
 }
